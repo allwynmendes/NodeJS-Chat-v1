2c4e696463c2640d1e79dafcbbe4e5de06bfaba7
chocomonsta <allwyn.mendes@gmail.com>
2017-04-23 14:01:09 +0530

    Finally working...Tested on Local Network

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
-
+ 
 // Initialize appication with route / (that means root of the application)
 app.get('/', function(req, res){
   var express=require('express');
   app.use(express.static(path.join(__dirname)));
   res.sendFile(path.join(__dirname, '../chat-application', 'index.html'));
 });
-
+ 
 // Register events on socket connection
 io.on('connection', function(socket){ 
   socket.on('chatMessage', function(from, msg){
