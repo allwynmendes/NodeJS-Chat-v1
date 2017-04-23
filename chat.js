12225a784bc7825f66af9ec3529548d87eec1010
chocomonsta <allwyn.mendes@gmail.com>
2017-04-23 13:57:14 +0530

    Issues...debugging

--- chat.js
+++ chat.js
@@ -40,6 +40,7 @@ function makeid() {
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
  
   for( var i=0; i < 5; i++ ) {
+	//The code below needs some explanation 
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
